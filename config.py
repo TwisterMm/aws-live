eb41866882a5e35add02de44afd57bdf10c2b854
TwisterMm <67315290+TwisterMm@users.noreply.github.com>
2022-08-30 14:31:36 +0800

    upload file

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee1.cis3y6v6kddm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teewenshi-employee"
+customregion = "us-east-1"
